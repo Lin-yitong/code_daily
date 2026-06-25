e6f6336fe37b9023b21b64601b4d3baf2cd3e21a
nalan-rongru <1660311839@qq.com>
2026-06-25 15:34:38 +0800

    文本分割器-python_code

---
+++ LangChain/tese27.py
@@ -0,0 +1,23 @@
+from langchain_text_splitters import PythonCodeTextSplitter
+
+# 字符串文档
+PYTHON_CODE = """
+def hello_world():
+    print("Hello World")")
+
+def hello_python():
+    print("Hello Python")
+"""
+
+# 分割器(python代码)
+splitter = PythonCodeTextSplitter(
+    chunk_size=50,
+    chunk_overlap=0,
+)
+
+# 创建文档，遵守分割器规则
+python_docs = splitter.create_documents([PYTHON_CODE])
+
+for doc in python_docs:
+    print('*' * 40)
+    print(doc)
