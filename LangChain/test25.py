3d87aa4af0708df7173862c0ce65c5e111c3c1eb
nalan-rongru <1660311839@qq.com>
2026-06-25 12:05:31 +0800

    文档加载器-markdown

--- LangChain/test25.py
+++ LangChain/test25.py
@@ -1,4 +1,4 @@
-from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyPDFLoader, UnstructuredMarkdownLoader
 from langchain_core.documents import Document
 
 # 手动定义的文档列表
@@ -21,7 +21,7 @@ documens = [
     )
 ]
 
-# 文档加载器
+# 文档加载器(PDF)
 loader = PyPDFLoader(file_path="../Docs/pdf/自然资源主题新媒体科普宣传的实践与探索.pdf")
 # 加载：生成文档列表
 docs = loader.load()
@@ -30,3 +30,17 @@ docs = loader.load()
 print(f"PDF文档总页数：{len(docs)}\n")
 print(f"第一页文本的内容（前200）是：\n{docs[0].page_content[:200]}\n")
 print(f"第一页的元数据字典是:\n{docs[0].metadata}\n")
+
+# 文档加载器
+md_loader = UnstructuredMarkdownLoader(
+    "../Docs/markdown/自然资源主题新媒体科普宣传的实践与探索.md",
+    # mode = "single"  # MD 加载器默认将文档加载为一个
+    mode = "elements"
+)
+# Document 列表
+docs = md_loader.load()
+
+print(f"MD文档总页数：{len(docs)}\n")
+print(f"第一页文档的内容是：\n{docs[0].page_content}\n")
+
+print(f"当前MD文档的所有分类：{set(document.metadata["category"] for document in docs )}\n")
