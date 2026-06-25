1d6407cfc531a16b0152384d3ce525d7dd7fa38b
nalan-rongru <1660311839@qq.com>
2026-06-25 15:13:07 +0800

    文本分割器-基于字符长度拆分

---
+++ LangChain/test26.py
@@ -0,0 +1,22 @@
+from langchain_community.document_loaders import UnstructuredMarkdownLoader
+from langchain_text_splitters import CharacterTextSplitter
+
+# single 模式, 只生成一个大文档
+loader = UnstructuredMarkdownLoader("../docs/markdown/自然资源主题新媒体科普宣传的实践与探索.md")
+# Doucument 列表
+data = loader.load()
+
+# 定义文本分割器
+text_spliter = CharacterTextSplitter(
+    separator="\n\n",       # 分割符，一般来说，有一个默认的分隔符优先级列表：["\n\n","\n"," "]
+    chunk_size=400,         # 块大小（参考标准，为了保证段落/句子完整，会超出此设定的大小）
+    chunk_overlap=50,       # 块重叠大小
+    length_function=len,    #测量字符长度的函数
+    is_separator_regex=False,#是否正则表达式描写分隔符
+)
+
+# 分割文档
+documents = text_spliter.split_documents(data)
+for doc in documents:
+    print("*"*30)
+    print(doc)
